d539b7ee30eadd52b8bc3cd35457a5eebe4725f4
synchrovision <hatano@catpow.info>
2024-10-06 00:40:27 +0900

    chore :up: lint

--- test/util.test.js
+++ test/util.test.js
@@ -1,13 +1,13 @@
 import {describe, expect, test} from '@jest/globals';
 import {bem} from 'catpow/util';
 
-describe('test util', () => {
-  test('bem', () => {
-	  const classes=bem('cp-test');
-     expect(classes()).toBe('cp-test');
-     expect(classes('is-active')).toBe('cp-test is-active');
-     expect(classes({'is-active':true})).toBe('cp-test is-active');
-     expect(classes({'is-active':false})).toBe('cp-test');
-     expect(classes(['is-active','is-visible'])).toBe('cp-test is-active is-visible');
-  });
+describe('test util',()=>{
+	test('bem',()=>{
+		const classes=bem('cp-test');
+		expect(classes()).toBe('cp-test');
+		expect(classes('is-active')).toBe('cp-test is-active');
+		expect(classes({'is-active':true})).toBe('cp-test is-active');
+		expect(classes({'is-active':false})).toBe('cp-test');
+		expect(classes(['is-active','is-visible'])).toBe('cp-test is-active is-visible');
+	});
 });
