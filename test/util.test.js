ab0016045aefbc820a22dff7d267f7ca2cd75575
synchrovision <hatano@catpow.info>
2024-10-06 00:35:51 +0900

    feat :+1: implements test

--- test/util.test.js
+++ test/util.test.js
@@ -1,34 +1,13 @@
-{
-  "name": "catpow",
-  "version": "1.0.0",
-  "description": "Various mudule package.",
-  "main": "src/index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "repository": {
-    "type": "git",
-    "url": "git+https://github.com/synchrovision/catpow-esm.git"
-  },
-  "keywords": [
-    "dom",
-    "bem",
-    "react",
-    "hooks",
-    "slider",
-    "animation",
-    "easing",
-    "color",
-    "convert"
-  ],
-  "author": "synchrovision",
-  "license": "MIT",
-  "bugs": {
-    "url": "https://github.com/synchrovision/catpow-esm/issues"
-  },
-  "homepage": "https://github.com/synchrovision/catpow-esm#readme",
-  "devDependencies": {
-    "@testing-library/react": "^16.0.1",
-    "jest": "^29.7.0"
-  }
-}
+import {describe, expect, test} from '@jest/globals';
+import {bem} from 'catpow/util';
+
+describe('test util', () => {
+  test('bem', () => {
+	  const classes=bem('cp-test');
+     expect(classes()).toBe('cp-test');
+     expect(classes('is-active')).toBe('cp-test is-active');
+     expect(classes({'is-active':true})).toBe('cp-test is-active');
+     expect(classes({'is-active':false})).toBe('cp-test');
+     expect(classes(['is-active','is-visible'])).toBe('cp-test is-active is-visible');
+  });
+});
