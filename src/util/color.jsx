c3096727f518eaf9fe17d43d0fa20db83d3f9aee
synchrovision <hatano@catpow.info>
2024-10-07 22:18:37 +0900

    fix :bug:

--- src/util/color.jsx
+++ src/util/color.jsx
@@ -1,5 +1,5 @@
 export const hexToRgb=(hex)=>{
-	const [r,g,b]=(hex.match(/#?(\w)(\w)(\w)$/) || hex.match(/#?(\w{2})(\w{2})(\w{2})$/)).slice(1).map((c)=>parseInt(c,16));
+	const [r,g,b]=(hex.match(/^#?(\w)(\w)(\w)$/) || hex.match(/^#?(\w{2})(\w{2})(\w{2})$/)).slice(1).map((c)=>parseInt(c,16));
 	return {r,g,b};
 };
 export const rgbToHex=(rgb)=>{
