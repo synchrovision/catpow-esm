2102bc7456fe03eae9dc3dc683f5b158ff88e8bd
synchrovision <hatano@catpow.info>
2024-10-07 22:19:54 +0900

    feat :+1: implements tests

--- test/util.test.js
+++ test/util.test.js
@@ -1,5 +1,10 @@
 import {describe, expect, test} from '@jest/globals';
-import {bem} from 'catpow/util';
+import {
+	bem,//bem
+	dataSizeStringToInt,intToDataSizeString,pfloor,pround,pceil,//calc
+	bsearch,range,//aray
+	hexToRgb,rgbToHex,hexToHsl,hslToHex,hexToHsb,hsbToHex,colorToHsla,//color
+} from 'catpow/util';
 
 describe('test util',()=>{
 	test('bem',()=>{
@@ -9,5 +14,31 @@ describe('test util',()=>{
 		expect(classes({'is-active':true})).toBe('cp-test is-active');
 		expect(classes({'is-active':false})).toBe('cp-test');
 		expect(classes(['is-active','is-visible'])).toBe('cp-test is-active is-visible');
+	});
+	test('calc',()=>{
+		expect(dataSizeStringToInt('1KB')).toBe(1024);
+		expect(dataSizeStringToInt('1MB')).toBe(1024*1024);
+		expect(dataSizeStringToInt('1GB')).toBe(1024*1024*1024);
+		expect(intToDataSizeString(1024)).toBe('1KB');
+		expect(intToDataSizeString(1024*1024)).toBe('1MB');
+		expect(intToDataSizeString(1024*1024*1024)).toBe('1GB');
+		expect(pfloor(0.123,2)).toBe(0.12);
+		expect(pround(0.123,2)).toBe(0.12);
+		expect(pceil(0.123,2)).toBe(0.13);
+		expect(pfloor(0.456,2)).toBe(0.45);
+		expect(pround(0.456,2)).toBe(0.46);
+		expect(pceil(0.456,2)).toBe(0.46);
+	});
+	test('array',()=>{
+		expect(bsearch([0,1,2,3,4,5],3)).toBe(3);
+		expect(bsearch([0,1,2,3,4,5],3.5)).toBe(3);
+		expect(Array.from(range(2,5))).toEqual([2,3,4,5]);
+		expect(Array.from(range(1,9,2))).toEqual([1,3,5,7,9]);
+	});
+	test('color',()=>{
+		expect(hexToRgb('#ff0000')).toEqual({r:255,g:0,b:0});
+		expect(rgbToHex({r:255,g:0,b:0})).toBe('#ff0000');
+		expect(hexToHsl('#FF0000')).toEqual({h:0,s:100,l:50});
+		expect(hexToHsl('#FFFFFF')).toEqual({h:0,s:0,l:100});
 	});
 });
