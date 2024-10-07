bc968e703a815edd7b1055e4bdeb87172adeca17
synchrovision <hatano@catpow.info>
2024-10-07 22:18:16 +0900

    fix :bug:

--- src/util/array.jsx
+++ src/util/array.jsx
@@ -1,9 +1,9 @@
 export const bsearch=(arr,val)=>{
-	let l=0,r=arr.length-1;
+	let l=0,r=arr.length-1,m;
 	if(arr[0]>val){return -1;}
 	if(arr[r]<val){return r+1;}
 	while(l<=r){
-		let m=l+((r-l)>>1);
+		m=l+((r-l)>>1);
 		if(arr[m]===val){return m;}
 		if(arr[m]>val){r=m-1;}
 		else{l=m+1;}
