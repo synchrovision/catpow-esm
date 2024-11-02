05be0b6e4d140cf4bbb1b65ee42f66126bbd6388
synchrovision <hatano@catpow.info>
2024-11-03 04:19:30 +0900

    feat :+1: add useCache hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 export * from './useBem.jsx';
+export * from './useCache.jsx';
 export * from './useQuery.jsx';
 export * from './useTransition.jsx';
---
+++ src/hooks/useCache.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const getMapRecursive=(map,keys)=>{
+	let current=map;
+	for(const key of keys){
+		if(!current.has(key)){current.set(key,new Map);}
+		current=current.get(key);
+	}
+	return current;
+}
+
+export const useCache=(callback,args)=>{
+	const {useMemo,useRef}=React;
+	const {current:cacheTree}=useRef(new Map);
+	
+	return useMemo(()=>{
+		const [primaryKey,...keys]=args;
+		const cache=getMapRecursive(cacheTree,args);
+		if(!cache.has(primaryKey)){cache.set(primaryKey,callback());}
+		return cache.get(primaryKey);
+	},args);
+}
