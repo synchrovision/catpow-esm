cd29b4fe15eb613980e7d7c22836360c587ff535
synchrovision <hatano@catpow.info>
2024-12-06 17:07:34 +0900

    feat 👍 add useLazyComponent hook

---
+++ src/hooks/useLazyComponent.jsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import {useCache} from './useCache.jsx';
+
+export const useLazyComponent=(Component,asyncCallback,args)=>{
+	const {useMemo,useCallback,lazy}=React;
+	
+	return useCache(()=>lazy(async()=>{
+		const staticProps=await asyncCallback(...args);
+		return {default:(props)=><Component {...staticProps} {...props}/>};
+	}),args);
+}
