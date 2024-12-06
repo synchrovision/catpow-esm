75808e2c2cde3e2bdbe0fb450fad50efc2d8137c
synchrovision <hatano@catpow.info>
2024-12-06 16:42:09 +0900

    feat 👍 add ucFirst and ucWords function

--- src/util/string.jsx
+++ src/util/string.jsx
@@ -7,3 +7,6 @@ export const camelToKebab=(str)=>str.replace(/(\w)([A-Z])/g,'$1-$2').toLowerCase
 export const camelToSnake=(str)=>str.replace(/(\w)([A-Z])/g,'$1_$2').toLowerCase();
 export const kebabToCamel=(str)=>str.replace(/\-(\w)/g,(m)=>m[1].toUpperCase());
 export const snakeToCamel=(str)=>str.replace(/_(\w)/g,(m)=>m[1].toUpperCase());
+
+export const ucFirst=(str)=>str.replace(/^([a-z])/,(m)=>m[1].toUpperCase());
+export const ucWords=(str)=>str.replace(/\b([a-z])/g,(m)=>m[1].toUpperCase());
