583965da993029729118d9a8ce7ba0996864baa3
synchrovision <hatano@catpow.info>
2025-02-01 22:00:11 +0900

    feat 👍 add getCharCategory

--- src/util/string.jsx
+++ src/util/string.jsx
@@ -10,3 +10,22 @@ export const snakeToCamel=(str)=>str.replace(/_(\w)/g,(m)=>m[1].toUpperCase());
 
 export const ucFirst=(str)=>str.replace(/^([a-z])/,(m)=>m[1].toUpperCase());
 export const ucWords=(str)=>str.replace(/\b([a-z])/g,(m)=>m[1].toUpperCase());
+
+export const getCharCategory=(chr)=>{
+	const codePoint=chr.charCodeAt(0);
+	if(codePoint >= 0x30 && codePoint <= 0x39) {return "number"; }
+	if(codePoint >= 0x41 && codePoint <= 0x5A){return "upper-alphabet"}
+	if(codePoint >= 0x61 && codePoint <= 0x7A){return "lower-alphabet"}
+	if(codePoint >= 0x3040 && codePoint <= 0x309F){return "hiragana";}
+	if(codePoint >= 0x30A0 && codePoint <= 0x30FF){return "katakana";}
+	if(codePoint >= 0x4E00 && codePoint <= 0x9FFF){return "kanji";}
+	if (
+		(codePoint >= 0x20 && codePoint <= 0x2F) || 
+		(codePoint >= 0x3A && codePoint <= 0x40) ||
+		(codePoint >= 0x5B && codePoint <= 0x60) ||
+		(codePoint >= 0x7B && codePoint <= 0x7E)
+	){
+		return "mark";
+	}
+	return "etc";
+}
