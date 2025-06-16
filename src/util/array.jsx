1ccdaa0b9fb5b937e20ff2ef29932b32856e60ff
synchrovision <hatano@catpow.info>
2025-06-16 20:13:27 +0900

    feat 👍 add combine function

--- src/util/array.jsx
+++ src/util/array.jsx
@@ -30,3 +30,12 @@ export const range = function* (start, end, step = 1) {
 		yield i;
 	}
 };
+export const combine = (keys, values) => {
+	if (keys.length !== values.length) {
+		throw new Error("Keys and values must have the same length");
+	}
+	return keys.reduce((obj, key, index) => {
+		obj[key] = values[index];
+		return obj;
+	}, {});
+};
