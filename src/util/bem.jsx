24f6e77ca1c6a50193b017409e0b854f9fac05f2
synchrovision <hatano@catpow.info>
2025-06-23 14:41:42 +0900

    feat 👍 consider SVGElement at applyBem

--- src/util/bem.jsx
+++ src/util/bem.jsx
@@ -77,32 +77,39 @@ export const applyBem = (el, { ...ctx }) => {
 		});
 		return el;
 	}
-	if (!(el instanceof HTMLElement)) {
+	if (!(el instanceof Element)) {
 		return el;
 	}
 	let { className } = el;
+	if (el instanceof SVGElement && className instanceof SVGAnimatedString) {
+		className = className.baseVal;
+	}
 	if (className) {
-		el.className = className
-			.split(" ")
-			.map((className) => {
-				if (className.slice(0, 2) === "--") {
-					return ctx.element + className;
-				}
-				if (className[0] === "-") {
-					return (ctx.block = ctx.element = ctx.block + className);
-				}
-				if (className[0] === "_") {
-					return (ctx.element = ctx.element + (ctx.element === ctx.block ? "__" : "-") + className.slice(1));
-				}
-				if (className.slice(-1) === "-") {
-					return (ctx.block = ctx.element = ctx.prefix + "-" + className.slice(0, -1));
-				}
-				if (className.slice(-1) === "_") {
-					return (ctx.element = ctx.block + "__" + className.slice(0, -1));
-				}
-				return className;
-			})
-			.join(" ");
+		console.log({ className });
+		el.setAttribute(
+			"class",
+			className
+				.split(" ")
+				.map((className) => {
+					if (className.slice(0, 2) === "--") {
+						return ctx.element + className;
+					}
+					if (className[0] === "-") {
+						return (ctx.block = ctx.element = ctx.block + className);
+					}
+					if (className[0] === "_") {
+						return (ctx.element = ctx.element + (ctx.element === ctx.block ? "__" : "-") + className.slice(1));
+					}
+					if (className.slice(-1) === "-") {
+						return (ctx.block = ctx.element = ctx.prefix + "-" + className.slice(0, -1));
+					}
+					if (className.slice(-1) === "_") {
+						return (ctx.element = ctx.block + "__" + className.slice(0, -1));
+					}
+					return className;
+				})
+				.join(" ")
+		);
 		if (el.className === className) {
 			const matches = className.match(/\b(([a-z]+)\-[a-z]+(\-[a-z]+)*)(__[a-z]+(\-[a-z]+)*)?\b/);
 			if (!matches) {
@@ -115,7 +122,7 @@ export const applyBem = (el, { ...ctx }) => {
 			ctx.element = matches[0];
 		}
 	} else {
-		el.className = ctx.element = ctx.element + (ctx.element === ctx.block ? "__" : "-") + el.tagName.toLowerCase();
+		el.setAttribute("class", (ctx.element = ctx.element + (ctx.element === ctx.block ? "__" : "-") + el.tagName.toLowerCase()));
 	}
 	for (let i = 0; i < el.children.length; i++) {
 		applyBem(el.children[i], ctx);
