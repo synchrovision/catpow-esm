f01fa5015365843d323183eb59b6d91721d6262d
synchrovision <hatano@catpow.info>
2025-07-08 17:16:04 +0900

    change 🔧 do not replace uppercase or digit at kebabToCamel and snakeToCamel

--- src/util/string.jsx
+++ src/util/string.jsx
@@ -36,8 +36,8 @@ export const filterFlags = (flags, callback) => {
 
 export const camelToKebab = (str) => str.replace(/(\w)([A-Z])/g, "$1-$2").toLowerCase();
 export const camelToSnake = (str) => str.replace(/(\w)([A-Z])/g, "$1_$2").toLowerCase();
-export const kebabToCamel = (str) => str.replace(/\-(\w)/g, (m) => m[1].toUpperCase());
-export const snakeToCamel = (str) => str.replace(/_(\w)/g, (m) => m[1].toUpperCase());
+export const kebabToCamel = (str) => str.replace(/\-([a-z])/g, (m) => m[1].toUpperCase());
+export const snakeToCamel = (str) => str.replace(/_([a-z])/g, (m) => m[1].toUpperCase());
 
 export const ucFirst = (str) => str.replace(/^([a-z])/, (m) => m[1].toUpperCase());
 export const ucWords = (str) => str.replace(/\b([a-z])/g, (m) => m[1].toUpperCase());
