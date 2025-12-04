af34756c75dc5d04dc3de3dca0c2e61195468c69
synchrovision <hatano@catpow.info>
2025-12-05 01:05:27 +0900

    chore 🆙 migration

--- src/component/functions.jsx
+++ src/component/functions.jsx
@@ -1,7 +1,10 @@
+import { createRoot } from "react-dom/client";
+import { createElement } from "react";
+
 export const renderComponents = () => {
 	const list = document.querySelectorAll("[data-component]");
 	Array.prototype.forEach.call(list, (el) => {
-		ReactDOM.render(React.createElement(window[el.dataset.component]), { ...el.dataset });
+		createRoot(el).render(createElement(window[el.dataset.component]), { ...el.dataset });
 	});
 };
 export const nl2br = (text) => {
