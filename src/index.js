f4e939e13a1da8740006c482357e284cafac7724
synchrovision <hatano@catpow.info>
2025-12-07 16:43:26 +0900

    edit 📝 update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+export * as app from "./app/index.js";
 export * as component from "./component/index.js";
+export * as dom from "./dom/index.js";
 export * as graphic from "./graphic/index.js";
 export * as hooks from "./hooks/index.js";
+export * as schema from "./schema/index.js";
+export * as scssc from "./scssc/index.js";
 export * as ui from "./ui/index.js";
 export * as util from "./util/index.js";
-export * as app from "./app/index.js";
-export * as schema from "./schema/index.js";
