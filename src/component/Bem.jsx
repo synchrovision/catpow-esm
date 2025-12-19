fa125975e741a8de70018731d97906bb5f4c80fd
synchrovision <hatano@catpow.info>
2025-12-19 17:46:52 +0900

    chore 🆙 remove debug code

--- src/component/Bem.jsx
+++ src/component/Bem.jsx
@@ -43,8 +43,6 @@ const applyBem = (component, { ...ctx }) => {
 			const matches =
 				(ctx.prefix && className.match(new RegExp(`\\b((${ctx.prefix.replaceAll("-", "\\-")})\\-[a-z]+(\\-[a-z]+)*)(__[a-z]+(\\-[a-z]+)*)?\\b`))) ||
 				className.match(/\b(([a-z]+)\-[a-z]+(\-[a-z]+)*)(__[a-z]+(\-[a-z]+)*)?\b/);
-			console.log(`\\b((${ctx.prefix.replaceAll("-", "\\-")})\\-[a-z]+(\\-[a-z]+)*)(__[a-z]+(\\-[a-z]+)*)?\\b`);
-			console.log(matches);
 			if (!matches) {
 				return;
 			}
