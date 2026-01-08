4043cbd063b1d8a9bc5c4dbb69029f1aac3e2f1b
synchrovision <hatano@catpow.info>
2026-01-08 14:19:35 +0900

    feat 👍 update SVG component

--- src/component/SVG/SVG.jsx
+++ src/component/SVG/SVG.jsx
@@ -1,32 +1,20 @@
-﻿import React from "react";
+﻿import { useMemo, createContext } from "react";
+
+export const SVGColorsContext = createContext({});
+export const SVGScreenContext = createContext({});
 
 export const SVG = (props) => {
-	const { className = "cp-svg", width = 1200, height = 400, colors, children, ...otherProps } = props;
-	const { useState, useMemo, useCallback, useEffect, useRef, useReducer } = React;
+	const { className = "cp-svg", width = 1200, height = 400, colors = { h: 20, s: 80, l: 80 }, children, ...otherProps } = props;
 
-	const { Colors, Screen } = useMemo(() => {
-		["Colors", "Screen"].forEach((contextName) => {
-			if (undefined === SVG[contextName]) {
-				SVG[contextName] = React.createContext({});
-			}
-		});
-		return SVG;
-	}, []);
-	const ColorsValue = useMemo(() => {
-		if (undefined !== colors) {
-			return colors;
-		}
-		return { h: 20, s: 80, l: 80 };
-	}, [colors]);
-	const ScreenValue = useMemo(() => ({ width, height }), [width, height]);
+	const ScreenValue = useMemo(() => ({ width, height, flex: false }), [width, height]);
 
 	return (
-		<Colors.Provider value={ColorsValue}>
-			<Screen.Provider value={ScreenValue}>
+		<SVGColorsContext.Provider value={colors}>
+			<SVGScreenContext.Provider value={ScreenValue}>
 				<svg className={className} width={width} height={height} viewBox={`0 0 ${width} ${height}`} xmlns="http://www.w3.org/2000/svg" {...otherProps}>
 					{children}
 				</svg>
-			</Screen.Provider>
-		</Colors.Provider>
+			</SVGScreenContext.Provider>
+		</SVGColorsContext.Provider>
 	);
 };
