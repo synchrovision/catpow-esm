2d0f5ca6b80fb46e6b144e3c4bc1e6d936144d01
synchrovision <hatano@catpow.info>
2026-01-13 18:15:31 +0900

    feat 👍 update Chart component

--- src/component/Chart/Chart.tsx
+++ src/component/Chart/Chart.tsx
@@ -1,102 +1,40 @@
 ﻿import * as React from "react";
-import { useState, useMemo, useCallback, createContext } from "react";
-import { rangeValueConverter, RangeValueConverterApp, range } from "catpow/util";
+import { useMemo, createContext, useCallback, useContext } from "react";
+import { DataSetContext } from "./DataSet";
 
-type point = {
-	x: number;
-	y: number;
-};
 type progressPoint = {
 	px: number;
 	py: number;
 };
-
 type ChartProps = {
 	width?: number;
 	height?: number;
 	margin?: number[];
-	steps?: { [key: number]: number } | RangeValueConverterApp;
-	snap?: boolean;
-	values?: number[][];
-	labels?: string[][];
 	getValueWithProgressPoint: (r: number, c: number, progressPoint: progressPoint) => number;
-	getCellWithProgressPoint: (progressPoint: progressPoint) => { r: number; c: number };
-	onChange?: (values: number[][]) => void;
+	getCellWithProgressPoint: (progressPoint: progressPoint, focusedRow: number | null) => { r: number; c: number };
 	children?: any;
 };
 export type ChartContextValue = {
 	width: number;
 	height: number;
 	margin: number[];
-	steps: RangeValueConverterApp;
-	labels: string[][];
-	values: number[][];
-	setValue: (r: number, c: number, value: number) => void;
-	setActiveCellValue: (value: number) => void;
 	setValueWithProgressPoint: (r: number, c: number, progressPoint: progressPoint) => void;
 	setActiveCellValueWithProgressPoint: (progressPoint: progressPoint) => void;
-	activeRow: number;
-	setActiveRow: (r: number) => void;
-	activeColumn: number;
-	setActiveColumn: (c: number) => void;
-	selectCell: (r: number, c: number) => void;
-	unselectCell: () => void;
 	selectCellWithProgressPoint: (progressPoint: progressPoint) => void;
 };
 
 export const ChartContext = createContext<ChartContextValue | null>(null);
 
 export const Chart = (props: ChartProps) => {
-	const {
-		width = 640,
-		height = 480,
-		margin = [4, 4, 4, 4],
-		steps = { 100: 1 },
-		snap = true,
-		labels,
-		values,
-		getValueWithProgressPoint,
-		getCellWithProgressPoint,
-		onChange,
-		children,
-		...otherProps
-	} = props;
+	const { width = 640, height = 480, margin = [4, 4, 4, 4], getValueWithProgressPoint, getCellWithProgressPoint, children, ...otherProps } = props;
+	const { selectCell, focusedRow, setValue, activeRow, activeColumn } = useContext(DataSetContext);
 
-	const [activeRow, setActiveRow] = useState(null);
-	const [activeColumn, setActiveColumn] = useState(null);
-	const selectCell = useCallback(
-		(r: number, c: number) => {
-			setActiveRow(r);
-			setActiveColumn(c);
-		},
-		[setActiveRow, setActiveColumn]
-	);
-	const unselectCell = useCallback(() => {
-		setActiveRow(null);
-		setActiveColumn(null);
-	}, [setActiveRow, setActiveColumn]);
 	const selectCellWithProgressPoint = useCallback(
 		(progressPoint: progressPoint) => {
-			const { r, c } = getCellWithProgressPoint(progressPoint);
+			const { r, c } = getCellWithProgressPoint(progressPoint, focusedRow);
 			selectCell(r, c);
 		},
-		[selectCell, getCellWithProgressPoint]
-	);
-
-	const setValue = useCallback(
-		(r: number, c: number, value: number) => {
-			const newValues = [...values];
-			newValues[r] = [...newValues[r]];
-			newValues[r][c] = value;
-			onChange(newValues);
-		},
-		[values, onChange]
-	);
-	const setActiveCellValue = useCallback(
-		(value: number) => {
-			setValue(activeRow, activeColumn, value);
-		},
-		[activeRow, activeColumn, setValue]
+		[selectCell, getCellWithProgressPoint, focusedRow]
 	);
 	const setValueWithProgressPoint = useCallback(
 		(r: number, c: number, progressPoint: progressPoint) => {
@@ -114,47 +52,16 @@ export const Chart = (props: ChartProps) => {
 		[activeRow, activeColumn, setValueWithProgressPoint]
 	);
 
-	const converter = useMemo(() => (steps.hasOwnProperty("getValue") ? steps : rangeValueConverter(steps, snap)), [steps, snap]);
-
 	const chartContextValue = useMemo<ChartContextValue>(
 		() => ({
 			width,
 			height,
 			margin,
-			steps: converter,
-			values,
-			labels,
-			setValue,
-			setActiveCellValue,
+			selectCellWithProgressPoint,
 			setValueWithProgressPoint,
 			setActiveCellValueWithProgressPoint,
-			activeRow,
-			setActiveRow,
-			activeColumn,
-			setActiveColumn,
-			selectCell,
-			unselectCell,
-			selectCellWithProgressPoint,
 		}),
-		[
-			width,
-			height,
-			margin,
-			converter,
-			values,
-			labels,
-			setValue,
-			setActiveCellValue,
-			setValueWithProgressPoint,
-			setActiveCellValueWithProgressPoint,
-			activeRow,
-			setActiveRow,
-			activeColumn,
-			setActiveColumn,
-			selectCell,
-			unselectCell,
-			selectCellWithProgressPoint,
-		]
+		[width, height, margin, selectCellWithProgressPoint, setValueWithProgressPoint, setActiveCellValueWithProgressPoint]
 	);
 
 	return <ChartContext.Provider value={chartContextValue}>{children}</ChartContext.Provider>;
