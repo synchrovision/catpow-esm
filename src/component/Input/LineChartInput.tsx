931258713fba92c8ab42864af9b177d3fcf8e17f
synchrovision <hatano@catpow.info>
2026-01-13 18:14:57 +0900

    feat 👍 update LineChartInput component

--- src/component/Input/LineChartInput.tsx
+++ src/component/Input/LineChartInput.tsx
@@ -1,18 +1,14 @@
 ﻿import * as React from "react";
-import { Bem } from "catpow/component";
-import { LineChart, LineChartProps } from "../Chart/LineChart";
-import { ChartInputBox } from "./ChartInputBox";
 
-export const LineChartInput = (props: LineChartProps) => {
-	const { className = "cp-linechartinput", children, ...otherProps } = props;
+import { LineChart } from "../Chart/LineChart";
+import { ChartInput } from "./ChartInput";
 
+export const LineChartInput = (props) => {
+	const { children, ...otherProps } = props;
 	return (
-		<Bem>
-			<div className={className} style={{ position: "relative" }}>
-				<LineChart className="-chart" {...otherProps}>
-					<ChartInputBox />
-				</LineChart>
-			</div>
-		</Bem>
+		<LineChart {...otherProps}>
+			<ChartInput />
+			{children}
+		</LineChart>
 	);
 };
