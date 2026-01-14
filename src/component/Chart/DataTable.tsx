ccafba72fa99c5c3a6113b543577a812c2a8330e
synchrovision <hatano@catpow.info>
2026-01-14 18:48:36 +0900

    feat 👍 add DataTable component

---
+++ src/component/Chart/DataTable.tsx
@@ -0,0 +1,43 @@
+﻿import * as React from "react";
+import { useMemo, useCallback, useContext } from "react";
+import { Bem, SVG } from "catpow/component";
+import { DataSetContext } from "./DataSet";
+import { clsx } from "clsx";
+
+type DataTableProps = {
+	className?: string;
+};
+
+export const DataTable = (props: DataTableProps) => {
+	const { className = "cp-datatabel", ...otherProps } = props;
+	const { labels, classNames, colors, values, getDisplayValue } = useContext(DataSetContext);
+
+	return (
+		<Bem>
+			<div className={className}>
+				<table>
+					{labels.columns && (
+						<thead>
+							<tr>
+								{labels.rows && <td></td>}
+								{labels.columns.map((label) => (
+									<th>{label}</th>
+								))}
+							</tr>
+						</thead>
+					)}
+					<tbody>
+						{values.map((row, r) => (
+							<tr>
+								{labels.rows && <th>{labels.rows[r]}</th>}
+								{row.map((v, c) => (
+									<td>{getDisplayValue(r, c)}</td>
+								))}
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</Bem>
+	);
+};
--- src/component/Chart/index.js
+++ src/component/Chart/index.js
@@ -1,6 +1,7 @@
 export * from "./Chart.tsx";
 export * from "./BarChart.tsx";
 export * from "./DataSet.tsx";
+export * from "./DataTable.tsx";
 export * from "./Legend.tsx";
 export * from "./LineChart.tsx";
 export * from "./PolarChart.tsx";
