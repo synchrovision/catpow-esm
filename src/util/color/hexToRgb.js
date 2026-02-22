c1c46d446c841a887dd32e59abb4fcff899b74fc
synchrovision <hatano@catpow.info>
2026-02-22 21:14:34 +0900

    feat 👍 update hexToRgb

--- src/util/color/hexToRgb.js
+++ src/util/color/hexToRgb.js
@@ -1,4 +1,7 @@
 export const hexToRgb = (hex) => {
-	const [r, g, b] = (hex.match(/^#?(\w)(\w)(\w)$/) || hex.match(/^#?(\w{2})(\w{2})(\w{2})$/)).slice(1).map((c) => parseInt(c, 16));
-	return { r, g, b };
+	const [r, g, b, a = 255] = (hex.match(/^#?(\w)(\w)(\w)(\w)?$/) || hex.match(/^#?(\w{2})(\w{2})(\w{2})(\w{2})?$/)).slice(1).map((c) => {
+		const val = parseInt(c, 16);
+		return isNaN(val) ? 255 : val;
+	});
+	return { r, g, b, a };
 };
