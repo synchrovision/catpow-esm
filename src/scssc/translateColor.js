26dd606f39da1b030639f4217b0323721712064a
synchrovision <hatano@catpow.info>
2026-02-25 14:12:56 +0900

    feat 👍 update translateColor

--- src/scssc/translateColor.js
+++ src/scssc/translateColor.js
@@ -23,10 +23,10 @@ export const translateColor = (color, tint, alpha) => {
 			const cf = (p) => `var(--cp-container-tones-${key}-${p})`;
 			const rf = (p) => `var(--cp-root-tones-${key}-${p})`;
 			const h = num ? (staticHue ? num : num === "0" || num === "6" ? f("h") : `calc(${relativeHue ? cf("h") : rf("h")} + var(--cp-tones-hr) * ${num - 6} + var(--cp-tones-hs))`) : f("h");
-			const s = f("s");
-			const l = tint ? `calc(100% - ${f("l")} * ${tint})` : `${f("l")}`;
+			const c = f("c");
+			const l = tint ? `calc(1 - ${f("t")} * ${tint} / 100)` : `${f("l")}`;
 			const a = alpha ? `calc(${f("a")} * ${alpha})` : f("a");
-			return `hsla(${h}, ${s}, ${l}, ${a})`;
+			return `oklch(${l} ${c} ${h} / ${a})`;
 		}
 	} else {
 		const matches = color.match(/^([a-z]+)\-([a-z]+)$/);
@@ -37,7 +37,7 @@ export const translateColor = (color, tint, alpha) => {
 				const t2 = 1 - t1;
 				const f = (p) => `calc(var(--cp-tones-${key1}-${p}) * ${t1} + var(--cp-tones-${key2}-${p}) * ${t2})`;
 				const a = alpha ? `calc(${f("a")} * ${alpha})` : f("a");
-				return `hsla(${f("h")}, ${f("s")}, ${f("l")}, ${a})`;
+				return `oklch(${f("l")} ${f("c")} ${f("h")} / ${a})`;
 			}
 		}
 	}
