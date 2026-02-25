69648306f1eadca74562660b97d8e22343730cae
synchrovision <hatano@catpow.info>
2026-02-25 14:23:01 +0900

    edit 📝 update PositionInput component

--- src/component/Input/PositionInput.jsx
+++ src/component/Input/PositionInput.jsx
@@ -22,11 +22,12 @@ export const PositionInput = (props) => {
 
 	useThrottle(() => onChange({ x: pos.x, y: pos.y }), 50, [pos.x, pos.y]);
 	useEffect(() => {
+		const { x: ox, y: oy, dx, dy, elW, elH } = state;
 		if (!state.isScratching) {
 			return;
 		}
-		let x = parseInt(Math.max(0, Math.min(state.x + state.dx - margin, width)));
-		let y = parseInt(Math.max(0, Math.min(state.y + state.dy - margin, height)));
+		let x = parseInt(Math.max(0, Math.min(((width + margin * 2) * (ox + dx)) / elW - margin, width)));
+		let y = parseInt(Math.max(0, Math.min(((height + margin * 2) * (oy + dy)) / elH - margin, height)));
 		if (isNaN(x) || isNaN(y)) {
 			return;
 		}
@@ -39,32 +40,32 @@ export const PositionInput = (props) => {
 
 	return (
 		<Bem>
-			<svg
-				className={className}
-				width={width + margin * 2}
-				height={height + margin * 2}
-				viewBox={`0 0 ${width + margin * 2} ${height + margin * 2}`}
-				xmlns="http://www.w3.org/2000/svg"
-				ref={ref}
-				style={{ cursor: state.isScratching ? "grabbing" : "grab" }}
-				{...otherProps}
-			>
-				<rect className="_bg" x={margin} y={margin} width={width} height={height} fill="none" stroke="currentColor" strokeOpacity={0.5} />
-				<circle fill="none" stroke="currentColor" strokeOpacity={0.75} className="_circle" cx={pos.x + margin} cy={pos.y + margin} r={r} />
-				<circle fill="currentColor" className="_dot" cx={pos.x + margin} cy={pos.y + margin} r={2} />
-				{grid > 0 && (
-					<>
-						{Array.from({ length: Math.floor(width / grid) }, (_, i) => (
-							<line key={i} stroke="currentColor" strokeOpacity={0.1} className="_grid" x1={i * grid + margin} y1={margin} x2={i * grid + margin} y2={height + margin} />
-						))}
-						{Array.from({ length: Math.floor(height / grid) }, (_, i) => (
-							<line key={i} stroke="currentColor" strokeOpacity={0.1} className="_grid" x1={margin} y1={i * grid + margin} x2={width + margin} y2={i * grid + margin} />
-						))}
-						<line stroke="currentColor" strokeOpacity={0.2} className="_grid is-center" x1={width / 2 + margin} y1={margin} x2={width / 2 + margin} y2={height + margin} />
-						<line stroke="currentColor" strokeOpacity={0.2} className="_grid is-center" x1={margin} y1={height / 2 + margin} x2={width + margin} y2={height / 2 + margin} />
-					</>
-				)}
-			</svg>
+			<div className={className} ref={ref} {...otherProps}>
+				<svg
+					className="_body"
+					width={width + margin * 2}
+					height={height + margin * 2}
+					viewBox={`0 0 ${width + margin * 2} ${height + margin * 2}`}
+					xmlns="http://www.w3.org/2000/svg"
+					style={{ cursor: state.isScratching ? "grabbing" : "grab" }}
+				>
+					<rect className="_bg" x={margin} y={margin} width={width} height={height} fill="none" stroke="currentColor" strokeOpacity={0.5} />
+					<circle fill="none" stroke="currentColor" strokeOpacity={0.75} className="_circle" cx={pos.x + margin} cy={pos.y + margin} r={r} />
+					<circle fill="currentColor" className="_dot" cx={pos.x + margin} cy={pos.y + margin} r={2} />
+					{grid > 0 && (
+						<>
+							{Array.from({ length: Math.floor(width / grid) }, (_, i) => (
+								<line key={i} stroke="currentColor" strokeOpacity={0.1} className="_grid" x1={i * grid + margin} y1={margin} x2={i * grid + margin} y2={height + margin} />
+							))}
+							{Array.from({ length: Math.floor(height / grid) }, (_, i) => (
+								<line key={i} stroke="currentColor" strokeOpacity={0.1} className="_grid" x1={margin} y1={i * grid + margin} x2={width + margin} y2={i * grid + margin} />
+							))}
+							<line stroke="currentColor" strokeOpacity={0.2} className="_grid is-center" x1={width / 2 + margin} y1={margin} x2={width / 2 + margin} y2={height + margin} />
+							<line stroke="currentColor" strokeOpacity={0.2} className="_grid is-center" x1={margin} y1={height / 2 + margin} x2={width + margin} y2={height / 2 + margin} />
+						</>
+					)}
+				</svg>
+			</div>
 		</Bem>
 	);
 };
